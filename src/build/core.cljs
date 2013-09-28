e971e613ba1b8154c6bafaee84cf64a646d50ab9
takuya <budoutou@ezweb.ne.jp>
2013-09-28 12:50:00 +0900

    最初のコミット

--- src/build/core.cljs
+++ src/build/core.cljs
@@ -1,12 +1,3 @@
-(ns build.core
-  (:require [pylon.classes])
-  (:use-macros [pylon.macros :only [defclass]]))
+(ns build.core)
 
-(defclass Hello
-  (defn constructor [name]
-    (set! @.name name))
-  (defn hello []
-    (console/log (str "Hello " @.name "!"))))
-
-(.hello (Hello. "Kitty"))
 
